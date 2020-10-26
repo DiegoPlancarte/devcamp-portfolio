29d633c3f612cd70c1c5a3bea5ee6c18774e4df5
Diego Plancarte <dplancarte7@gmail.com>
2020-10-26 14:29:53 -0700

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,8 @@
 class Portfolio < ApplicationRecord
+  # Imports placeholder.rb concern helper methods
+  include Placeholder
+
+  # Checks that values are present before saving instance to backend
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   # Custom Scope : Option 1
@@ -11,8 +15,9 @@ class Portfolio < ApplicationRecord
 
   after_initialize :set_defaults
 
+  # Setting default values: '||= value' used in order to not overwrite values and only replace nil values.
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
