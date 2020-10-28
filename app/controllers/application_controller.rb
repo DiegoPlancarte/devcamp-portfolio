2338e9d7c95dc92ed33070745ef13ff4b71ab8e0
Diego Plancarte <dplancarte7@gmail.com>
2020-10-28 10:49:45 -0700

    Added controller concerns for devise params.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  # Runs specified methods before running anything else, specifies anything related to devise
-  before_action :configure_permitted_marameters, if: :devise_controller?
-
-  # Telling devise to allow custom fields on new user registration and user registration edit
-  def configure_permitted_marameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist
 end
